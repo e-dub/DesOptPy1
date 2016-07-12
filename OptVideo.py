159ad3ab8288f1413650e8e37d182154d6ae35c7
Wehrle <Wehrle.Erich@gmail.com>
2016-07-12 09:42:32 +0200

    line length PEP8

--- OptVideo.py
+++ OptVideo.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 '''
-----------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 Title:          OptVideo.py
 Units:          Unitless
 Date:           October 16, 2013
 Author:         E.J. Wehrle
-----------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 
-----------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 Description
-----------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 Optimization suite for Python...
 
 TODO several videos for system responses
@@ -18,12 +18,16 @@ TODO check codec
 TODO is mencorder on cluster, master, workstations?
 TODO integrate in file OptPostProc?
 TODO save pngs in result folder? Yes!
+-------------------------------------------------------------------------------
 '''
 
 import os
-def OptVideo(OptName):
-    fps=4
-    #cd in _OptResultReports/OptName
 
-    os.system("mencoder 'mf://DesVar***.png' -mf type=png:fps="+str(4)+" -ovc lavc -lavcopts vcodec=wmv2 -oac copy -o DesVar.mpg")
-    os.system("mencoder 'mf://SysRes***.png' -mf type=png:fps="+str(4)+" -ovc lavc -lavcopts vcodec=wmv2 -oac copy -o SysRes.mpg")
+
+def OptVideo(OptName):
+    fps = 4
+    # cd in _OptResultReports/OptName
+    os.system("mencoder 'mf://DesVar***.png' -mf type=png:fps=" + str(fps) +
+              " -ovc lavc -lavcopts vcodec=wmv2 -oac copy -o DesVar.mpg")
+    os.system("mencoder 'mf://SysRes***.png' -mf type=png:fps=" + str(fps) +
+              " -ovc lavc -lavcopts vcodec=wmv2 -oac copy -o SysRes.mpg")
